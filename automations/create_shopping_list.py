b7693c38a8b34e2f8e3ce10bb89ea793baa0f62d
Zachary Povey <64191599+zachary-povey@users.noreply.github.com>
2024-11-07 22:43:14 +0100

    wip shopping list

--- automations/create_shopping_list.py
+++ automations/create_shopping_list.py
@@ -1,13 +1,17 @@
 import os
-from typing import List
+from typing import List, TypedDict
 import requests
-import json
 from dataclasses import dataclass
 from datetime import datetime
+import frontmatter
+
+script_dir = os.path.abspath(os.path.dirname(__file__))
+content_dir = os.path.abspath(os.path.join(script_dir, "..", "content"))
 
 
 @dataclass
 class Config:
+    meal_plan: str
     trello_api_key: str
     trello_api_token: str
     trello_board_id: str = "BFSQpAhe"
@@ -18,12 +22,24 @@ class Config:
     def from_env(cls):
         try:
             return cls(
+                meal_plan=os.environ.get("MEAL_PLAN", "2024-11-01"),
                 trello_api_key=os.environ["TRELLO_KEY"],
                 trello_api_token=os.environ["TRELLO_TOKEN"],
             )
         except KeyError as e:
             raise ValueError(f"Missing required environment variable: {e}")
+    
+    @property
+    def meal_plan_path(self):
+        return os.path.join(
+            content_dir,
+            "meal_plans",
+            f"{self.meal_plan}.md",
+        )
 
+class IngredientQuantity(TypedDict):
+    units: str | None
+    quantity: float
 
 #  specify meal plan
 #  fetch front matter yaml from each meal in plan
@@ -100,11 +116,17 @@ def create_checklist_in_trello(card_id: str, name: str, config: Config) -> str:
     response.raise_for_status()
     return response.json()["id"]
 
+def get_raw_ingredients(config: Config) -> List[dict]:
+    #  {"ing": [{"units": "g", "name": "flour", "quantity": 100}]}
+    meal_plan = frontmatter.load(config.meal_plan_path).to_dict()
+    pass
 
-def main():
+def main(meal_plan: str):
     config = Config.from_env()
-    create_list_in_trello(["eggs", "milk", "flour"], config)
+    raw_ingredients = get_raw_ingredients(config)
+    # create_list_in_trello(["eggs", "milk", "flour"], config)
 
 
 if __name__ == "__main__":
-    main()
+    meal_plan = os.getenv("MEAL_PLAN", "2024-11-01")
+    main(meal_plan)
